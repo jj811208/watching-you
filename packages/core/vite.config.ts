221d74a78117ea761ebba06db0658868f18f1d4a
ryan_chu <jj811208@gmail.com>
2022-09-10 19:30:02 +0800

    chore: pre-publish preparation

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'gazer',
-      fileName: 'gazer'
+      fileName: 'gazer',
     },
   },
 });
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
