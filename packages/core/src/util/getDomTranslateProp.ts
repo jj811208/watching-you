20b6ffbffee4f2b758cda3159cce974da0a9c003
ryan_chu <jj811208@gmail.com>
2022-09-14 23:46:24 +0800

    feat: make `getDomTranslateProp` support `happy-dom` environment

--- packages/core/src/util/getDomTranslateProp.ts
+++ packages/core/src/util/getDomTranslateProp.ts
@@ -1,9 +1,15 @@
-// 有優化空間
+// the `domTransform` format we get from our test environment is different from the browser's format, which is a compatibility writing method
+const splitRegex = /px\) rotate\(|translate\(|deg\)|px,/;
 function getDomTranslateProp(dom: HTMLElement) {
   const domTransform = getComputedStyle(dom).transform;
   if (domTransform === 'none') return { x: 0, y: 0 };
-  const result = getComputedStyle(dom)
-    .transform.slice(0, -1)
+  if (domTransform.includes('translate')) {
+    const result = domTransform.split(splitRegex);
+    return { x: Number(result[1]), y: Number(result[2]) };
+  }
+  // the way faster then `new DOMMatrix` or `regex`
+  const result = domTransform
+    .slice(0, -1)
     .split(', ')
     .slice(-2)
     .map(Number);
