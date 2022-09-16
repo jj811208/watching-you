f4f306e2016f9b74a5e417a3791e734aa4e9664b
ryan_chu <jj811208@gmail.com>
2022-09-17 00:14:20 +0800

    feat: make the core demo ez test performance

--- packages/core/src/demo/index.ts
+++ packages/core/src/demo/index.ts
@@ -3,16 +3,39 @@ import WatchingYou from '../index';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="section">
     <h1>WatchingYou Demo</h1>
-    <div class="eyes">
-      <div>O</div>
-    </div>
+    ${new Array(8)
+      .fill('')
+      .map(
+        () => `<div style="display: flex; gap: 12px;">
+          ${new Array(25)
+            .fill('')
+            .map(() => `<div class="eyes"><div>O</div></div>`)
+            .join('')}
+        </div>
+        `,
+      )
+      .join('')}
   </div>
 `;
 
-const watchingYou = new WatchingYou('.eyes > div', {
-  power: {
-    x: 22,
-    y: 42,
-  },
+const allWatcher = document.querySelectorAll('.eyes > div');
+const watchingYouList: any[] = [];
+
+allWatcher.forEach((watcher) => {
+  const watchingYou = new WatchingYou(watcher, {
+    power: {
+      x: 22,
+      y: 42,
+    },
+  });
+  watchingYou.start();
+  watchingYouList.push(watchingYou);
 });
-watchingYou.start();
+
+const cancelButton = document.querySelector('#app > div > h1');
+
+cancelButton?.addEventListener('click', () => {
+  watchingYouList.forEach((watchingYou) => {
+    watchingYou.cancel();
+  });
+});
