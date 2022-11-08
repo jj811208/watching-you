9bb56ad370b73f2c7bf89c7421f59d23c27464d1
ryan_chu <jj811208@gmail.com>
2022-11-08 23:15:56 +0800

    fix: lint warning

--- packages/website/src/Route.tsx
+++ packages/website/src/Route.tsx
@@ -1,33 +1,11 @@
 import {
   createHashRouter,
-  Link,
   RouterProvider,
 } from 'react-router-dom';
-import githubIconUrl from './PNG/github-icon.png';
 import Login from './examples/login';
 import Cena from './examples/cena';
 import IronChain from './examples/iron-chain';
-import styled from 'styled-components';
-import WatchingYou from 'react-watching-you';
 import Landing from './Landing';
-
-const ExampleList = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  padding: 24px;
-
-  a {
-    font-size: 32px;
-  }
-`;
-
-const Menu = styled.div`
-  display: flex;
-  gap: 16px;
-  padding: 16px 32px;
-  background: #ccc;
-`;
 
 const routeList = [
   {
